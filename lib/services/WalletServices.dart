1f8b6d6fdb8242835b8021a3e5209ba43d0166b9
Michael Angelo De Leon <mad@mad.local>
2021-01-27 02:19:48 +0800

    transfer balance to rider

--- lib/services/WalletServices.dart
+++ lib/services/WalletServices.dart
@@ -6,6 +6,7 @@ import 'package:global_configuration/global_configuration.dart';
 import 'package:kp_mobile/model/user/GetBalanceModel.dart';
 import 'package:http/http.dart' as http;
 import 'package:kp_mobile/model/user/TopupModel.dart';
+import 'package:kp_mobile/model/user/TransferModel.dart';
 
 class WalletServices {
   Future<GetBalanceModel> getBalance({
@@ -41,6 +42,29 @@ class WalletServices {
       },
     ).then(
       (res) => TopupModel.fromJson(
+        json.decode(res.body),
+      ),
+    );
+  }
+
+  Future<TransferModel> transfer({
+    @required String token,
+    @required String amount,
+    @required String riderId, 
+  }) async {
+    return await http.post(
+      '${GlobalConfiguration().get('api_url')}wallet/transfer/rider',
+      headers: {
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      },
+      body: {
+        'amount': amount,
+        'rider_id': riderId,
+      },
+    ).then(
+      (res) => TransferModel.fromJson(
         json.decode(res.body),
       ),
     );
