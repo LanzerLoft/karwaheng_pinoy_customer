fe0161c278a314d59411a040d0cbb964fe995150
Michael Angelo De Leon <mad@mad.local>
2021-01-27 02:03:07 +0800

    get wallet balance added

---
+++ lib/services/WalletServices.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:kp_mobile/model/user/GetBalanceModel.dart';
+import 'package:http/http.dart' as http;
+
+class WalletServices {
+  Future<GetBalanceModel> getBalance({
+    @required String token,
+  }) async {
+    return await http.post(
+      '${GlobalConfiguration().get('api_url')}wallet/balance',
+      headers: {
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      },
+    ).then(
+      (res) => GetBalanceModel.fromJson(
+        json.decode(res.body),
+      ),
+    );
+  }
+}
